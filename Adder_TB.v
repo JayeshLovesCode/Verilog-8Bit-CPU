c5981ee0b5059c168617fbebbdd8ef89e82d4165
VTrajah <venoththeone@gmail.com>
2026-02-01 22:50:29 -0500

    Adder TB Created, shifters syntax fixed with a tab

---
+++ Adder_TB.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+module Adder_TB;
+    //Testbench signals 
+    reg [7:0] A_sim, B_sim;
+    reg Carry_in_sim;
+    wire [7:0] Sum_sim;
+    wire Carry_out;
+    Adder dut(.A(A_sim), .B(B_sim), .Carry_in(Carry_in_sim), .Sum(Sum_sim), Carry_out(Carry_out_sim));
+
+    //Dumpfile and dumpvariable  for waveform generaotr
+    initial begin
+        $dumpfile("Adder_TB.vcd"); // dumpfile name
+        $dumpvars(0,Adder_TB); // ) 0 is dumping previous signals
+    end
+
+    //Applying stimulus for generator 
+    initial begin
+        A_sim = b'8 10110000
+        B_sim = b'8 01001000
+        Carry_in_sim = 0
+        #10 
+        A_sim = b'8 10001000
+        B_sim = b'8 11001000
+        Carry_in_sim = 0
+        #10 
+        A_sim = b'8 11110000
+        B_sim = b'8 11010000
+        Carry_in_sim = 0
+        #10 
+        A_sim = b'8 11000000
+        B_sim = b'8 00101000
+        Carry_in_sim = 1
+        #10 
+        A_sim = b'8 10010111
+        B_sim = b'8 11011000
+        Carry_in_sim = 1
+        #10 
+        A_sim = b'8 10101100
+        B_sim = b'8 11111010
+        Carry_in_sim = 1
+        #10 
+        $finish;
+    end
+    initial begin
+        $monitor($time, "A_sim = %d, B_sim = %d, Carry_in_sim = %b, Sum_sim = %d, Carry_out_sim = %b,", A_sim, B_sim, Carry_in_sim, Sum_sim, Carry_out_sim);
+    end
+
+endmodule
+
--- Shifter_to_right_TB.v
+++ Shifter_to_right_TB.v
@@ -1,23 +1,23 @@
 `timescale 1ns / 1ps
 module Shifter_to_right_TB;
-reg [7:0] in_sim;
-wire [7:0] out_sim;
-wire  carry_out_sim;
-Shifter_to_right dut(.in_bit(in_sim), .out_bit(out_sim), .carry_out(carry_out_sim));
+    reg [7:0] in_sim;
+    wire [7:0] out_sim;
+    wire  carry_out_sim;
+    Shifter_to_right dut(.in_bit(in_sim), .out_bit(out_sim), .carry_out(carry_out_sim));
 
-initial begin
-    $dumpfile("Shifter_to_right_TB.vcd");
-    $dumpvars(0, Shifter_to_right_TB);
-end
+    initial begin
+        $dumpfile("Shifter_to_right_TB.vcd");
+        $dumpvars(0, Shifter_to_right_TB);
+    end
 
-initial begin
-    in_sim = 8'b 10001000;
-    #10 in_sim = 8'b 11100000;
-    #10 in_sim = 8'b 11010111;
-    #10 in_sim = 8'b 01011110;
-    #20 $finish;
-end
-initial begin
-    $monitor($time, "in_sim = %b, out_bit = %b, carry_out = %b", in_sim, out_sim, carry_out_sim);
-end
+    initial begin
+        in_sim = 8'b 10001000;
+        #10 in_sim = 8'b 11100000;
+        #10 in_sim = 8'b 11010111;
+        #10 in_sim = 8'b 01011110;
+        #20 $finish;
+    end
+    initial begin
+        $monitor($time, "in_sim = %b, out_bit = %b, carry_out = %b", in_sim, out_sim, carry_out_sim);
+    end
 endmodule
