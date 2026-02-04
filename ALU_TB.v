0e3847d620c8070dbfd18bc9c93e7825cc4b2418
VTrajah <venoththeone@gmail.com>
2026-02-03 23:25:01 -0500

             $monitor($time, "A_sim = %d, B_sim = %d, Carry_in_sim = %b, Sum_sim = %d, Carry_out_sim = %b,", A_sim, B_sim, Carry_in_sim, Sum_sim, Carry_out_sim);     end

--- ALU_TB.v
+++ ALU_TB.v
@@ -12,5 +12,23 @@ module ALU_TB;
     wire [3:0] NZCV_sim;
     ALU dut(.A(A_sim), .B(B_sim), .OP_Code(OP_Code_sim), .Result(Result_sim), .NZCV(NZCV_sim));
 
+    initial begin
+        $dumpfile("ALU_TB.vcd");
+        $dumpvars(0, ALU_TB);
+    end
+
+    // Stimulus for generator 
+    initial begin
+        //Test Case for 
+        A_sim = 00000000;
+        B_sim = 00000000;
+        OP_Code_sim = 000;
+    end
+
+    initial begin
+        $monitor($time, "A_sim = %b, B_sim = %b, OP_Code_sim = %b, Result_sim = %b, NZCV_sim = %b,", A_sim, B_sim, OP_Code_sim, Result_sim, NZCV_sim);
+    end
+
+
 
 endmodule
