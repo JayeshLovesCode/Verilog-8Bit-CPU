67dbcbc91e4b667045f25bd153c62b5e88b26b64
VTrajah <venoththeone@gmail.com>
2026-02-04 23:34:09 -0500

    started to complete test bench

--- ALU_TB.v
+++ ALU_TB.v
@@ -1,5 +1,5 @@
 // version  1.0.0 
-// last edited 2026-02-03 , 11:24 PM
+// last edited 2026-02-04 , 11:30 PM
 // last edited by Venoth
 
 //Rough ALU Test Bench 
@@ -25,47 +25,60 @@ module ALU_TB;
         OP_Code_sim = 000;
         #10
 
-        //Test Case for OP_Code 001
-        A_sim = 00000000;
+        A_sim = 11100100;
+        B_sim = 10100010;
+        OP_Code_sim = 000;
+        #10
+
+        A_sim = 11100100;
         B_sim = 00000000;
         OP_Code_sim = 000;
         #10
 
+        A_sim = 00000000;
+        B_sim = 11100100;
+        OP_Code_sim = 000;
+        #10
+
+
+        //Test Case for OP_Code 001
+        A_sim = 00000000;
+        B_sim = 00000000;
+        OP_Code_sim = 001;
+        #10
+
+
         //Test Case for OP_Code 011
         A_sim = 00000000;
         B_sim = 00000000;
-        OP_Code_sim = 000;
+        OP_Code_sim = 011;
         #10
 
         //Test Case for OP_Code 100
         A_sim = 00000000;
         B_sim = 00000000;
-        OP_Code_sim = 000;
+        OP_Code_sim = 100;
         #10
 
         //Test Case for OP_Code 101
         A_sim = 00000000;
         B_sim = 00000000;
-        OP_Code_sim = 000;
+        OP_Code_sim = 101;
         #10
 
         //Test Case for OP_Code 110
         A_sim = 00000000;
         B_sim = 00000000;
-        OP_Code_sim = 000;
-        #10
-
-        //Test Case for OP_Code 110
-        A_sim = 00000000;
-        B_sim = 00000000;
-        OP_Code_sim = 000;
+        OP_Code_sim = 110;
         #10
 
         //Test Case for OP_Code 111
         A_sim = 00000000;
         B_sim = 00000000;
-        OP_Code_sim = 000;
+        OP_Code_sim = 111;
         #10
+
+        $finish
 
     end
 
